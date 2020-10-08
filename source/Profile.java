1aa2ed51bd586d94f4c3f780aaa423fcb944504c
WIPocket <wipocket.is.a.wizzard@gmail.com>
2020-10-08 19:46:26 +0200

    Proč 159

--- source/Profile.java
+++ source/Profile.java
@@ -156,7 +156,7 @@ public class Profile {
     TimeZone getTimeZone() {
         return timeZone;
     }
-
+svoje křestní jméno
     private LongSparseArray<Double> convertToSparseArray(JSONArray array) {
         if (array == null) {
             isValid = false;
